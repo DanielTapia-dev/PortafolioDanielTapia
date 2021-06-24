c74c529f35c9bc4797591b01e10a880e61d775c9
Daniel <d.tapialopez1@gmail.com>
2021-06-23 23:06:37 -0500

    Correccion de problemas con paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './pages/inicio/inicio.component';
 
 const routes: Routes = [
-  { path: 'inicio', component: InicioComponent },
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    InicioComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
