3385c0435d98e7eac34e9f6e9ef4ec251e67c723
Daniel <d.tapialopez1@gmail.com>
2021-06-25 20:56:16 -0500

    Mejora del fondo en tiempo

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,14 @@ module.exports = {
         'luna': "url('https://res.cloudinary.com/latamarket/image/upload/v1624468505/luna_aghhs8.png')",
         'estrellas': "url('https://res.cloudinary.com/latamarket/image/upload/v1624510634/stars_xrkrjt.png')",
         'estrellas-fugaz': "url('https://res.cloudinary.com/latamarket/image/upload/v1624513512/estrellafugaz1_p5efuk.png')",
-        'fondo-espacio': "url('https://res.cloudinary.com/latamarket/image/upload/v1624515949/FondoEspacial_remspv.png')",
+        'fondo-espacio': "url(/assets/img/FondoEspacial_remspv.png)",
         'nave-espacial': "url('https://res.cloudinary.com/latamarket/image/upload/v1624516120/NaveEspacial_trz1lx.png')",
       }),
       animation: {
         'bg-luna': 'backLuna 50s linear infinite',
         'bg-luna-md': 'backLunaMD 50s linear infinite',
         'bg-estrella-fugaz': 'backEstrellaFugaz 10s linear infinite',
-        'bg-fondo-espacio': 'backEspacio 90s linear infinite',
+        'bg-fondo-espacio': 'backEspacio 900s linear infinite',
         'bg-nave-espacial': 'backNaveEspacial 40s linear infinite',
         'bg-nave-espacial-xs': 'backNaveEspacialXS 40s linear infinite'
       },
@@ -47,7 +47,7 @@ module.exports = {
         },
         backEspacio: {
           '0%': { 'background-position': '0px 0px' },
-          '100%': { 'background-position': '-1000px -800px' },
+          '100%': { 'background-position': '-10000px -8000px' },
         },
         backNaveEspacial: {
           '0%': { transform: "translate(4000px,2000px)" },
