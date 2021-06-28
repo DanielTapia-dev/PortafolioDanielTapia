8bcab64e9dbb2b82c4853b46bb0a048fccd8e971
Daniel <d.tapialopez1@gmail.com>
2021-06-28 11:43:38 -0500

    ¨Mejorando el header

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,53 +7,124 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  @Input() start: boolean = true;
-  @Input() about: boolean = false;
-  @Input() projects: boolean = false;
-  @Input() contact: boolean = false;
-
+  start: any = 'true';
+  about: any = 'false';
+  projects: any = 'false';
+  contact: any = 'false';
   seccion: any;
+  page: string = '';
 
-  constructor() { }
+  constructor() {
+    if (!localStorage.getItem('page')) {
+      this.CheckControlPages();
+      window.location.href = ('/');
+    } else {
+      this.CheckControlPages();
+    }
+  }
+
 
   ngOnInit(): void {
   }
 
+  //Funcion que mueve la pantall
   MoveScreen(screen: string) {
     switch (screen) {
       case 'start':
-        this.start = true;
-        this.about = false;
-        this.projects = false;
-        this.contact = false;
+        localStorage.setItem('page', 'start');
+        this.CheckControlPages();
         break;
       case 'about':
-        this.start = false;
-        this.about = true;
-        this.projects = false;
-        this.contact = false;
+        localStorage.setItem('page', 'about');
+        this.CheckControlPages();
         break;
       case 'projects':
-        this.start = false;
-        this.about = false;
-        this.projects = true;
-        this.contact = false;
+        localStorage.setItem('page', 'projects');
+        this.CheckControlPages();
         break;
       case 'contact':
-        this.start = false;
-        this.about = false;
-        this.projects = false;
-        this.contact = true;
+        localStorage.setItem('page', 'contact');
+        this.CheckControlPages();
         break;
       default:
         break;
     }
+    console.log(screen);
     this.seccion = document.getElementById(screen);
-    this.seccion.scrollIntoView({ block: "end", behavior: "smooth" });
+    console.log(this.seccion.getBoundingClientRect().height);
+    window.scrollTo({ left: 0, top: this.seccion.getBoundingClientRect().height, behavior: 'smooth' });
   }
 
-  @HostListener('mousewheel', ['$event']) onMousewheel(event: any) {
-    console.log(event.deltaY);
+  //Funcion para almacenar pagina activa en localstorage y en las variables
+  CheckControlPages() {
+    if (!localStorage.getItem('page')) {
+      localStorage.setItem('page', 'start');
+    } else {
+      if (localStorage.getItem('page') == 'start') {
+        this.start = 'true';
+        this.about = 'false';
+        this.projects = 'false';
+        this.contact = 'false';
+      } else if (localStorage.getItem('page') == 'about') {
+        this.start = 'false';
+        this.about = 'true';
+        this.projects = 'false';
+        this.contact = 'false';
+      } else if (localStorage.getItem('page') == 'projects') {
+        this.start = 'false';
+        this.about = 'false';
+        this.projects = 'true';
+        this.contact = 'false';
+      } else {
+        this.start = 'false';
+        this.about = 'false';
+        this.projects = 'false';
+        this.contact = 'true';
+      }
+    }
+  }
+
+  //Funcion para activar cambio de pantall al mover el scroll
+  @HostListener('wheel', ['$event']) onMousewheel(event: any) {
+    if (localStorage.getItem('page') == 'start') {
+      if (event.deltaY > '0') {
+        this.page = 'about';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'about');
+        this.CheckControlPages();
+      }
+    } else if (localStorage.getItem('page') == 'about') {
+      if (event.deltaY > '0') {
+        this.page = 'projects';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'projects');
+        this.CheckControlPages();
+      } else {
+        this.page = 'start';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'start');
+        this.CheckControlPages();
+      }
+    } else if (localStorage.getItem('page') == 'projects') {
+      if (event.deltaY > '0') {
+        this.page = 'contact';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'contact');
+        this.CheckControlPages();
+      } else {
+        this.page = 'about';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'about');
+        this.CheckControlPages();
+      }
+    } else if (localStorage.getItem('page') == 'contact') {
+      if (event.deltaY < '0') {
+        this.page = 'projects';
+        this.MoveScreen(this.page);
+        localStorage.setItem('page', 'projects');
+        this.CheckControlPages();
+      }
+    }
   }
 
 }
