ec2c3a279bfcff98e8d9c558d663fb2766f66632
Daniel <d.tapialopez1@gmail.com>
2021-07-09 02:01:54 -0500

    Eventos para gestos en telefonos

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -53,7 +53,7 @@ export class PagesComponent implements OnInit {
       let deltaTime = event.timeStamp - this.defaultTouch.time;
 
       // simulte a swipe -> less than 500 ms and more than 60 px
-      if (deltaTime < 300) {
+      if (deltaTime < 500) {
         if (Math.abs(deltaY) > 40) {
           // delta y is at least 60 pixels
           if (this.chargetime === true) {
