fd54a5144ff95dc77d182c9ed45c4657be8c237a
Daniel <d.tapialopez1@gmail.com>
2021-07-12 04:14:40 -0500

    ¨Pagina de contactos casi completa falta el link de whatsapp para enviar mensaje directo desde navegador

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+
   }
 
+  ngOnInit(): void {
+
+  }
+
+  sendToPage(page: string) {
+    switch (page) {
+      case 'facebook':
+        window.open("https://www.facebook.com/Da4le", "_self");
+        break;
+      case 'linkedin':
+        window.open("https://www.linkedin.com/in/daniel-tapia-5687b91b4/", "_self");
+        break;
+      case 'instagram':
+        window.open("https://www.instagram.com/dannyalejo7/", "_self");
+        break;
+      case 'whatsapp':
+        window.open("https://www.facebook.com/Da4le", "_self");
+        break;
+      default:
+        break;
+    }
+  }
 }
